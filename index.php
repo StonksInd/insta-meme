7614574788f05d49a5c840a90fd31198c6ce8256
Nath <nathlej41@gmail.com>
2024-03-13 11:17:02 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,34 @@
 <?php
 require_once 'php/affichage.php';
 require_once 'php/db.php';
+//recup le nombre de contenus
+$count = db()->prepare("SELECT count(id) as cpt from contenus ");
+$count->setFetchMode(PDO::FETCH_ASSOC);
+$count->execute();
+$tcount=$count->fetchAll();
 
+//Pagination
+@$page=$_GET["page"];
+if(empty($page)) $page=1;
+$nb_elt_page = 10;
+$nb_pages= ceil($tcount[0]["cpt"]/$nb_elt_page);
+$debut=($page-1)* $nb_elt_page;
+
+//Recupere le contenu
 $stmt = db()->prepare("SELECT contenus.*, utilisateurs.pseudo
 FROM contenus 
 JOIN utilisateurs ON  contenus.id_utilisateur = utilisateurs.id 
-GROUP BY contenus.id ");
+GROUP BY contenus.id
+LIMIT $debut, $nb_elt_page ");
 $stmt->execute();
 $contenus = $stmt->fetchAll();
+if(count($contenus)==0)
+    header("location:./");
 ?>
+
+
+
+
 
 <?php echo pageHeader("Menu Insta Meme");?>
 <link rel="stylesheet" href="css/style.css">
@@ -51,5 +71,13 @@ $contenus = $stmt->fetchAll();
     ?> 
     
 </main>
+<?php
+for($i;$i<=$nb_pages;$i++){
+    if($page!=$i)
+        echo "<a href='?page=$i'>$i</a>&nbsp;";
+    else
+        echo "<a>$i</a>&nbsp;";
+}
 
+?> 
 <?php echo pageFooter(); ?>
